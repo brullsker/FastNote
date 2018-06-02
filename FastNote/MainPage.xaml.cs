838c96887e3ef92aaf50457d5c4456909d56e998
Jakob Hermann Preusker <jakohe@outlook.de>
2018-06-02 20:49:25 +0200

    Worked on printing Added support of .txt and .html files Fixed a bug in relation with .rtf exporting

--- FastNote/MainPage.xaml.cs
+++ FastNote/MainPage.xaml.cs
@@ -19,6 +19,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.Helpers;
+using Microsoft.Toolkit.Uwp.UI.Helpers;
+using Windows.UI.Text;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
 
@@ -116,12 +119,48 @@ namespace FastNote
             MainView.IsPaneOpen = false;
         }
 
+
+
         private async void MoreOptionsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (MoreOptionsList.SelectedIndex == 0)
             {
                 MessageDialog md = new MessageDialog("Printing has not been implemented yet", "Not implemented");
                 await md.ShowAsync();
+
+                string converted = ConvertToHtml(MainEdit);
+
+                await ApplicationData.Current.LocalFolder.CreateFileAsync("html.html", CreationCollisionOption.ReplaceExisting);
+                StorageFile html = await ApplicationData.Current.LocalFolder.GetFileAsync("html.html");
+                await Windows.Storage.FileIO.WriteTextAsync(html, converted);
+
+                var grid = new Grid();
+                var webview = new WebView();
+                grid.Children.Add(webview);
+                Uri uri = new Uri("ms-appdata:///local/html.html");
+                webview.Source = uri;
+
+                var printHelper = new PrintHelper(grid);
+                Debug.WriteLine("Printhelper created");
+                printHelper.AddFrameworkElementToPrint(webview);
+                Debug.WriteLine("Frameworkelement added");
+                printHelper.OnPrintFailed += PrintHelper_OnPrintFailed;
+                printHelper.OnPrintSucceeded += PrintHelper_OnPrintSucceeded;
+                await printHelper.ShowPrintUIAsync("FastNote Print");
+                async void PrintHelper_OnPrintSucceeded()
+                {
+                    printHelper.Dispose();
+                    var dialog = new MessageDialog("Printing done.");
+                    await dialog.ShowAsync();
+                }
+
+                async void PrintHelper_OnPrintFailed()
+                {
+                    printHelper.Dispose();
+                    var dialog = new MessageDialog("Printing failed.");
+                    await dialog.ShowAsync();
+                }
+
             }
             if (MoreOptionsList.SelectedIndex == 1)
             {
@@ -152,9 +191,27 @@ namespace FastNote
                 picker.DefaultFileExtension = ".rtf";
                 picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
                 picker.FileTypeChoices.Add("Rich Text", new List<string>() { ".rtf" });
-                picker.SuggestedFileName = "FastNoteExport";
+                picker.FileTypeChoices.Add("HTML page", new List<string>() { ".html" });
+                picker.FileTypeChoices.Add("Plain text", new List<string>() { ".txt" });
+                picker.SuggestedFileName = "FastNote Export";
                 StorageFile saveFile = await picker.PickSaveFileAsync();
-                if (saveFile != null) await file.CopyAndReplaceAsync(saveFile);
+                if (saveFile != null)
+                {
+                    if (saveFile.FileType == ".rtf")
+                    {
+                        MainEdit.RequestedTheme = ElementTheme.Light;
+                        await file.CopyAndReplaceAsync(saveFile);
+                        if (Settings.Default.ThemeDefault == true) MainEdit.RequestedTheme = ElementTheme.Default;
+                        if (Settings.Default.ThemeDark == true) MainEdit.RequestedTheme = ElementTheme.Dark;
+                        if (Settings.Default.ThemeLight == true) MainEdit.RequestedTheme = ElementTheme.Light;
+                    }
+                    if (saveFile.FileType == ".html") await FileIO.WriteTextAsync(saveFile, ConvertToHtml(MainEdit));
+                    if (saveFile.FileType == ".txt")
+                    {
+                        MainEdit.Document.GetText(TextGetOptions.None, out string txtstring);
+                        await FileIO.WriteTextAsync(saveFile, txtstring);
+                    }
+                }
                 else
                 {
                     MessageDialog md = new MessageDialog("File wasn't saved.", "Operation cancelled");
@@ -219,6 +276,155 @@ namespace FastNote
         {
             if (MainEdit.Document.Selection.Length == 0) { ShareSelectedTextContent.Visibility = Visibility.Collapsed; ShareWholeTextContent.Visibility = Visibility.Visible; }
             else { ShareWholeTextContent.Visibility = Visibility.Collapsed; ShareSelectedTextContent.Visibility = Visibility.Visible; }
+        }
+        public static string ConvertToHtml(RichEditBox richEditBox)
+        {
+            string text, strColour, strFntName, strHTML;
+            richEditBox.Document.GetText(TextGetOptions.None, out text);
+            ITextRange txtRange = richEditBox.Document.GetRange(0, text.Length);
+            strHTML = "<html>";
+            int lngOriginalStart = txtRange.StartPosition;
+            int lngOriginalLength = txtRange.EndPosition;
+            float shtSize = 11;
+            // txtRange.SetRange(txtRange.StartPosition, txtRange.EndPosition);
+            bool bOpened = false, liOpened = false, numbLiOpened = false, iOpened = false, uOpened = false, bulletOpened = false, numberingOpened = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                txtRange.SetRange(i, i + 1);
+                if (i == 0)
+                {
+                    strColour = Windows.UI.Colors.Black.ToHex().ToString();
+                    shtSize = txtRange.CharacterFormat.Size;
+                    strFntName = txtRange.CharacterFormat.Name;
+                    strHTML += "<span style=\"font-family:" + strFntName + "; font-size: " + shtSize + "pt; color: #" + strColour.Substring(3) + "\">";
+                }
+                if (txtRange.CharacterFormat.Size != shtSize)
+                {
+                    shtSize = txtRange.CharacterFormat.Size;
+                    strHTML += "</span><span style=\"font-family: " + txtRange.CharacterFormat.Name + "; font-size: " + txtRange.CharacterFormat.Size + "pt; color: #" + txtRange.CharacterFormat.ForegroundColor.ToString().Substring(3) + "\">";
+                }
+                if (txtRange.Character == Convert.ToChar(13))
+                {
+                    strHTML += "<br/>";
+                }
+                #region bullet
+                if (txtRange.ParagraphFormat.ListType == MarkerType.Bullet)
+                {
+                    if (!bulletOpened)
+                    {
+                        strHTML += "<ul>";
+                        bulletOpened = true;
+                    }
+
+                    if (!liOpened)
+                    {
+                        strHTML += "<li>";
+                        liOpened = true;
+                    }
+
+                    if (txtRange.Character == Convert.ToChar(13))
+                    {
+                        strHTML += "</li>";
+                        liOpened = false;
+                    }
+                }
+                else
+                {
+                    if (bulletOpened)
+                    {
+                        strHTML += "</ul>";
+                        bulletOpened = false;
+                    }
+                }
+                #endregion
+                #region numbering
+                if (txtRange.ParagraphFormat.ListType == MarkerType.LowercaseRoman)
+                {
+                    if (!numberingOpened)
+                    {
+                        strHTML += "<ol type=\"i\">";
+                        numberingOpened = true;
+                    }
+
+                    if (!numbLiOpened)
+                    {
+                        strHTML += "<li>";
+                        numbLiOpened = true;
+                    }
+
+                    if (txtRange.Character == Convert.ToChar(13))
+                    {
+                        strHTML += "</li>";
+                        numbLiOpened = false;
+                    }
+                }
+                else
+                {
+                    if (numberingOpened)
+                    {
+                        strHTML += "</ol>";
+                        numberingOpened = false;
+                    }
+                }
+                #endregion
+                #region bold
+                if (txtRange.CharacterFormat.Bold == FormatEffect.On)
+                {
+                    if (!bOpened)
+                    {
+                        strHTML += "<b>";
+                        bOpened = true;
+                    }
+                }
+                else
+                {
+                    if (bOpened)
+                    {
+                        strHTML += "</b>";
+                        bOpened = false;
+                    }
+                }
+                #endregion
+                #region italic
+                if (txtRange.CharacterFormat.Italic == FormatEffect.On)
+                {
+                    if (!iOpened)
+                    {
+                        strHTML += "<i>";
+                        iOpened = true;
+                    }
+                }
+                else
+                {
+                    if (iOpened)
+                    {
+                        strHTML += "</i>";
+                        iOpened = false;
+                    }
+                }
+                #endregion
+                #region underline
+                if (txtRange.CharacterFormat.Underline == UnderlineType.Single)
+                {
+                    if (!uOpened)
+                    {
+                        strHTML += "<u>";
+                        uOpened = true;
+                    }
+                }
+                else
+                {
+                    if (uOpened)
+                    {
+                        strHTML += "</u>";
+                        uOpened = false;
+                    }
+                }
+                #endregion
+                strHTML += txtRange.Character;
+            }
+            strHTML += "</span></html>";
+            return strHTML;
         }
     }
 }
