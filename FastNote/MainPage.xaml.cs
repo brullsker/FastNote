brullsker/FastNote

commit 0e80a4627fb72470dadfa7799a2843bc7cae3e4c

Event handlers for rtf, html, txt sharing implemented

FastNote/MainPage.xaml
@@ -107,18 +107,18 @@
                                             <AppBarSeparator/>
                                             <TextBlock Text="SHARE FILE" Style="{StaticResource BaseTextBlockStyle}" VerticalAlignment="Center"/>
                                         </StackPanel>
-                                        <Button Grid.Column="2" HorizontalAlignment="Right" Width="44" Height="44">
+                                        <Button BorderThickness="0" Grid.Column="2" HorizontalAlignment="Right" Width="44" Height="44">
                                             <FontIcon FontFamily="Segoe MDL2 Assets" Glyph="&#xE097;" />
                                             <Button.Flyout>
                                                 <MenuFlyout Placement="Right">
-                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="Rich Text (.rtf)" Tag="&#xE160;"/>
-                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="HTML page (.html)" Tag="&#xE12B;"/>
-                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="Plain text (.txt)" Tag="&#xE1A2;"/>
-                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="JPG image (.jpg)" Tag="&#xE187;"/>
-                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="Portable Networks Graphics (.png)" Tag="&#xE2AC;"/>
-                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="Bitmap (.bmp)" Tag="&#xE80A;"/>
-                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="GIF image (.gif)" Tag="&#xE173;"/>
-                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="TIFF image (.tiff)" Tag="&#xE15A;"/>
+                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="Rich Text (.rtf)" Tag="&#xE160;" x:Name="ShareRTF" Click="ShareRTF_Click"/>
+                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="HTML page (.html)" Tag="&#xE12B;" x:Name="ShareHTML" Click="ShareHTML_Click"/>
+                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="Plain text (.txt)" Tag="&#xE1A2;" x:Name="ShareTXT" Click="ShareTXT_Click"/>
+                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="JPG image (.jpg)" Tag="&#xE187;" x:Name="ShareJPG" Click="ShareJPG_Click"/>
+                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="Portable Networks Graphics (.png)" Tag="&#xE2AC;" x:Name="SharePNG" Click="SharePNG_Click"/>
+                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="Bitmap (.bmp)" Tag="&#xE80A;" x:Name="ShareBMP" Click="ShareBMP_Click"/>
+                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="GIF image (.gif)" Tag="&#xE173;" x:Name="ShareGIF" Click="ShareGIF_Click"/>
+                                                    <MenuFlyoutItem Style="{StaticResource MenuFlyoutItemIconStyle}" Text="TIFF image (.tiff)" Tag="&#xE15A;" x:Name="ShareTIFF" Click="ShareTIFF_Click"/>
                                                 </MenuFlyout>
                                             </Button.Flyout>
                                         </Button>

FastNote/MainPage.xaml.cs
@@ -173,15 +173,10 @@ namespace FastNote
                         using (var stream = await saveFile.OpenStreamForWriteAsync())
                         {
                             var logicalDpi = DisplayInformation.GetForCurrentView().LogicalDpi;
-                            Debug.WriteLine("LogicalDPI set");
                             var pixelBuffer = await renderTargetBitmap.GetPixelsAsync();
-                            Debug.WriteLine("GetPixelAsync() done");
                             var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream.AsRandomAccessStream());
-                            Debug.WriteLine("BitmapEncoder created");
                             encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Straight, (uint)renderTargetBitmap.PixelWidth, (uint)renderTargetBitmap.PixelHeight, logicalDpi, logicalDpi, pixelBuffer.ToArray());
-                            Debug.WriteLine("PixelData set");
                             await encoder.FlushAsync();
-                            Debug.WriteLine("Flush");
                         }
                         if (Settings.Default.ThemeDefault == true) MainEdit.RequestedTheme = ElementTheme.Default;
                         if (Settings.Default.ThemeDark == true) MainEdit.RequestedTheme = ElementTheme.Dark;
@@ -276,7 +271,15 @@ namespace FastNote
                 LoadingControl.IsLoading = true;
 
                 StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.CreateFolderAsync("share", CreationCollisionOption.ReplaceExisting);
-                StorageFile sharefile = await file.CopyAsync(folder, Settings.Default.DefaultShareName + ".rtf", NameCollisionOption.ReplaceExisting);
+
+                if (Settings.Default.ShareFileType == 0) ShareRTF_Click(sender, e);
+                if (Settings.Default.ShareFileType == 1) ShareHTML_Click(sender, e);
+                if (Settings.Default.ShareFileType == 2) ShareTXT_Click(sender, e);
+                if (Settings.Default.ShareFileType == 3) ShareJPG_Click(sender, e);
+                if (Settings.Default.ShareFileType == 4) SharePNG_Click(sender, e);
+                if (Settings.Default.ShareFileType == 5) ShareBMP_Click(sender, e);
+                if (Settings.Default.ShareFileType == 6) ShareGIF_Click(sender, e);
+                if (Settings.Default.ShareFileType == 7) ShareTIFF_Click(sender, e);
 
                 IReadOnlyList<StorageFile> pickedFiles = await folder.GetFilesAsync();
 
@@ -532,6 +535,60 @@ namespace FastNote
             string charcount;
             MainEdit.Document.GetText(TextGetOptions.None, out charcount);
             CharCount.Text = Convert.ToString(charcount.Length - 1);
+        }
+
+        private async void ShareRTF_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.ShareFileType = 0;
+            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
+            StorageFile sharefile = await file.CopyAsync(folder, Settings.Default.DefaultShareName + ".rtf", NameCollisionOption.ReplaceExisting);
+        }
+
+        private async void ShareHTML_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.ShareFileType = 1;
+            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
+            StorageFile saveFile = await folder.CreateFileAsync(Settings.Default.DefaultShareName + "html");
+            MainEdit.Document.GetText(TextGetOptions.None, out string txtstring);
+            await FileIO.WriteTextAsync(saveFile, txtstring);
+        }
+
+        private async void ShareTXT_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.ShareFileType = 2;
+            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
+            StorageFile saveFile = await folder.CreateFileAsync(Settings.Default.DefaultShareName + "txt");
+            await FileIO.WriteTextAsync(saveFile, ConvertToHtml(MainEdit));
+        }
+
+        private async void ShareJPG_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
+
+        }
+
+        private async void SharePNG_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
+
+        }
+
+        private async void ShareBMP_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
+
+        }
+
+        private async void ShareGIF_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
+
+        }
+
+        private async void ShareTIFF_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
+
         }
     }
 }

FastNote/Settings.cs
@@ -59,7 +59,7 @@ namespace FastNote
         }
 
         [DefaultSettingValue(Value = 0)]
-        public int DefaultExportFileType
+        public int ShareFileType
         {
             get { return Get<int>(); }
             set { Set(value); }

commit d0460c8fa59738137d8b8ce03cfb59c07c5db869

Fixed bugs caused by commit 0e80a462

FastNote/MainPage.xaml
@@ -91,7 +91,7 @@
                                         </StackPanel>
                                     </Grid>
                                 </ListViewItem>
-                                <ListViewItem Margin="0,6" Background="{ThemeResource SystemControlBackgroundChromeMediumBrush}" Padding="12,0,0,0">
+                                <ListViewItem Margin="0,6" Background="{ThemeResource SystemControlBackgroundChromeMediumBrush}" Padding="12,0,0,0" x:Name="ShareItem">
                                     <Grid HorizontalAlignment="Stretch" Width="284">
                                         <Grid.ColumnDefinitions>
                                             <ColumnDefinition Width="Auto"/>

FastNote/MainPage.xaml.cs
@@ -272,9 +272,21 @@ namespace FastNote
 
                 StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.CreateFolderAsync("share", CreationCollisionOption.ReplaceExisting);
 
-                if (Settings.Default.ShareFileType == 0) ShareRTF_Click(sender, e);
-                if (Settings.Default.ShareFileType == 1) ShareHTML_Click(sender, e);
-                if (Settings.Default.ShareFileType == 2) ShareTXT_Click(sender, e);
+                if (Settings.Default.ShareFileType == 0)
+                {
+                    StorageFile sharefile = await file.CopyAsync(folder, Settings.Default.DefaultShareName + ".rtf", NameCollisionOption.ReplaceExisting);
+                }
+                if (Settings.Default.ShareFileType == 1)
+                {
+                    StorageFile shareFile = await folder.CreateFileAsync(Settings.Default.DefaultShareName + ".html", CreationCollisionOption.ReplaceExisting);
+                    await FileIO.WriteTextAsync(shareFile, ConvertToHtml(MainEdit));
+                }
+                if (Settings.Default.ShareFileType == 2)
+                {
+                    StorageFile shareFile = await folder.CreateFileAsync(Settings.Default.DefaultShareName + ".txt", CreationCollisionOption.ReplaceExisting);
+                    MainEdit.Document.GetText(TextGetOptions.None, out string txtstring);
+                    await FileIO.WriteTextAsync(shareFile, txtstring);
+                }
                 if (Settings.Default.ShareFileType == 3) ShareJPG_Click(sender, e);
                 if (Settings.Default.ShareFileType == 4) SharePNG_Click(sender, e);
                 if (Settings.Default.ShareFileType == 5) ShareBMP_Click(sender, e);
@@ -537,28 +549,22 @@ namespace FastNote
             CharCount.Text = Convert.ToString(charcount.Length - 1);
         }
 
-        private async void ShareRTF_Click(object sender, RoutedEventArgs e)
+        private void ShareRTF_Click(object sender, RoutedEventArgs e)
         {
             Settings.Default.ShareFileType = 0;
-            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
-            StorageFile sharefile = await file.CopyAsync(folder, Settings.Default.DefaultShareName + ".rtf", NameCollisionOption.ReplaceExisting);
+            ShareItem.Focus(FocusState.Pointer);
         }
 
-        private async void ShareHTML_Click(object sender, RoutedEventArgs e)
+        private void ShareHTML_Click(object sender, RoutedEventArgs e)
         {
             Settings.Default.ShareFileType = 1;
-            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
-            StorageFile saveFile = await folder.CreateFileAsync(Settings.Default.DefaultShareName + "html");
-            MainEdit.Document.GetText(TextGetOptions.None, out string txtstring);
-            await FileIO.WriteTextAsync(saveFile, txtstring);
+            ShareItem.Focus(FocusState.Pointer);
         }
 
-        private async void ShareTXT_Click(object sender, RoutedEventArgs e)
+        private void ShareTXT_Click(object sender, RoutedEventArgs e)
         {
             Settings.Default.ShareFileType = 2;
-            StorageFolder folder = await ApplicationData.Current.LocalCacheFolder.GetFolderAsync("share");
-            StorageFile saveFile = await folder.CreateFileAsync(Settings.Default.DefaultShareName + "txt");
-            await FileIO.WriteTextAsync(saveFile, ConvertToHtml(MainEdit));
+            ShareItem.Focus(FocusState.Pointer);
         }
 
         private async void ShareJPG_Click(object sender, RoutedEventArgs e)