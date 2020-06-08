d1f3a8cfe61e31fdc5fa14d0b01e2824055ad587
Jakob Hermann Preusker <jakohe@outlook.de>
2020-06-09 00:27:10 +0200

    updated gitignore

--- FastNote/Constants.cs
+++ FastNote/Constants.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 namespace FastNote
 {
     public class Constants
-    {        
-        public string SfKey = "Get one yourself";
+    {
+        public string SfKey = "MTkzMDg5QDMxMzcyZTM0MmUzMGRFS3JEZFExd0cwN2wySmZHa3ZDRlNYME9nM29xQmhoeHJ1TndlbHR2Kzg9";
 
-        static string storeid01 = "leave_alone";
-        static string storeid02 = "leave_alone";
-        static string storeid03 = "leave_alone";
-        static string storeid04 = "leave_alone";
-        static string storeid05 = "leave_alone";
-        static string storeid06 = "leave_alone";
+        static string storeid01 = "9PGVLJHM39HK";
+        static string storeid02 = "9PFTZQ520ZLH";
+        static string storeid03 = "9PDD8LSQ4W2Q";
+        static string storeid04 = "9PHKXXG943N2";
+        static string storeid05 = "9N7214FXLS49";
+        static string storeid06 = "9PNNTW5G9ZK7";
 
         public string[] storeIdArray = new string[6] { storeid01, storeid02, storeid03, storeid04, storeid05, storeid06 };
     }
