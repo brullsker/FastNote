1c8d0952a99199098e5effe804762a1cd9368f81
Jakob Hermann Preusker <jakohe@outlook.de>
2020-06-09 00:13:50 +0200

    Forgot to purge some advertisement remainders. They're gone for good now.

--- FastNote/Constants.cs
+++ FastNote/Constants.cs
@@ -7,24 +7,15 @@ using System.Threading.Tasks;
 namespace FastNote
 {
     public class Constants
-    {
-        //public string SfKey = "Get one yourself";
+    {        
+        public string SfKey = "Get one yourself";
 
-        //public string appid = "3f83fe91-d6be-434d-a0ae-7351c5a997f1"; // Test App ID
-        //public string adid = "test"; //Test Ad ID
-
-        
-        public string SfKey = "MTkzMDg5QDMxMzcyZTM0MmUzMGRFS3JEZFExd0cwN2wySmZHa3ZDRlNYME9nM29xQmhoeHJ1TndlbHR2Kzg9";
-
-        public string appid = "9n5grr8757vq"; //App ID
-        public string adid = "1100034498"; //Ad ID
-
-        static string storeid01 = "9PGVLJHM39HK";
-        static string storeid02 = "9PFTZQ520ZLH";
-        static string storeid03 = "9PDD8LSQ4W2Q";
-        static string storeid04 = "9PHKXXG943N2";
-        static string storeid05 = "9N7214FXLS49";
-        static string storeid06 = "9PNNTW5G9ZK7";
+        static string storeid01 = "leave_alone";
+        static string storeid02 = "leave_alone";
+        static string storeid03 = "leave_alone";
+        static string storeid04 = "leave_alone";
+        static string storeid05 = "leave_alone";
+        static string storeid06 = "leave_alone";
 
         public string[] storeIdArray = new string[6] { storeid01, storeid02, storeid03, storeid04, storeid05, storeid06 };
     }
